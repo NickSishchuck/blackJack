ff4519381edd43a788da0917e5c36e800d126ac1
NickSishchuck <mikiitasisuk@gmail.com>
2025-03-25 13:38:44 +0200

    Fixed the building process

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ mkdir -p builds
 # Build for Windows
 echo "Building Windows target..."
 cargo build --target x86_64-pc-windows-gnu --release
-cp target/x86_64-pc-windows-gnu/release/black-jak.exe builds/
+cp target/x86_64-pc-windows-gnu/release/black-jack.exe builds/
 
 # Build for Linux
 echo "Building Linux target..."
 cargo build --target x86_64-unknown-linux-gnu --release
-cp target/x86_64-unknown-linux-gnu/release/black-jak builds/black-jak-linux
+cp target/x86_64-unknown-linux-gnu/release/black-jack builds/black-jack-linux
 
 
 echo "Build complete! Binaries are in the 'builds' directory"
