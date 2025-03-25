835a56ec6d9f05a481d7eeb53e835ec90583e54d
NickSishchuck <mikiitasisuk@gmail.com>
2025-03-25 11:02:29 +0200

    Learned how to import files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::io;
+include!("test.rs");
+
+    fn main(){
+        test_message();
+      let mut input = String::new();
+      println!("Enter your name: ");
+      io::stdin().read_line(&mut input).expect("Failed to read line"); // we need to expect errors rithtaway
+      println!("Your name is: {}",input);
+    }
+
